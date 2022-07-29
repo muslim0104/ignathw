73bc03b94dbe13e94e5e2356d6345c17ff373cad
muslim0104 <muslimmagamadov95123@gmail.com>
2022-07-29 20:10:18 +0300

    Changes

--- src/p2-homeworks/h5-rrd-v6/HW5.tsx
+++ src/p2-homeworks/h5-rrd-v6/HW5.tsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import Header from './Header'
 import Pages from './Pages'
-import {BrowserRouter, HashRouter} from "react-router-dom";
+import { HashRouter} from "react-router-dom";
 
 
 function HW5() {
     return (
         <div>
             {/*в gh-pages лучше работает HashRouter*/}
-            <BrowserRouter>
+            <HashRouter>
 
 
             <Header/>
 
             <Pages/>
 
-            </BrowserRouter>
+            </HashRouter>
         </div>
     )
 }
