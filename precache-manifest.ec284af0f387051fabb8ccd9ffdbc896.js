3dbd32a6ee036b074154613d8d99305642e64a75
muslim0104 <muslimmagamadov95123@gmail.com>
2022-07-29 19:59:57 +0300

    Updates

---
+++ precache-manifest.ec284af0f387051fabb8ccd9ffdbc896.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "481ccbf68289c7a6efacb24bb83904e8",
+    "url": "/ignathw/index.html"
+  },
+  {
+    "revision": "d6ab5c0cc47510fd7454",
+    "url": "/ignathw/static/css/main.9cc6aed5.chunk.css"
+  },
+  {
+    "revision": "3e2f923f5ba45ccc8f4b",
+    "url": "/ignathw/static/js/2.d7874916.chunk.js"
+  },
+  {
+    "revision": "248fe9facfff898840212404b4cb88d9",
+    "url": "/ignathw/static/js/2.d7874916.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6ab5c0cc47510fd7454",
+    "url": "/ignathw/static/js/main.3b09de2e.chunk.js"
+  },
+  {
+    "revision": "3ceb7351a4ade46b5c7b",
+    "url": "/ignathw/static/js/runtime-main.16fbde4e.js"
+  }
+]);
